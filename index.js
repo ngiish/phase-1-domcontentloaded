6f629621a625b9cce5ab71416fa33fa9b610a57e
Isaac <govegeku00@gmail.com>
2023-09-30 20:11:16 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// // Your code goes here
+// document.addEventListener("DOMContentLoaded", ()=> {
+//     document.getElementById("text").textContent= "This is really cool"
+
+//      console.log ("This is really cool")
+// })
+document.addEventListener("DOMContentLoaded", () => {
+    const statementElement = document.querySelector('p')
+
+    statementElement.textContent = "This is really cool!"
+})
